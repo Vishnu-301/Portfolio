1ecf648f0533ff7aa93ddd61d2f28a0a0e389489
felix <ifyifeanyi301@gmail.com>
2026-03-14 09:00:51 +0100

    addition: made a few updates to my portholio

---
+++ public/js/portfolio.js
@@ -0,0 +1,60 @@
+const projects = [
+    {
+        title: "Job location site",
+        description: "A job finder workspace to find, secure and create your job according to your taste. Created with laravel, alpinejs and tailwind for styling and responsiveness and postgresql for database management.",
+        image: "public/img/workopia-full.png",
+        link: "portfolio.html#project1"
+    },
+    {
+        title: "BuiltBizz blog",
+        description: "Simple blog site created with raw vanilla php, mysql database javascript for search functionality and bootstrap for styling.",
+        image: "public/img/built-1.png",
+        link: "portfolio.html#project2"
+    },
+    {
+        title: "AgriAI",
+        description: "A job finder workspace to find, secure and create your job according to your taste. Created with laravel, alpinejs and tailwind for styling and responsiveness and postgresql for database management.",
+        image: "public/img/agriai.png",
+        link: "https://agriai-da00.onrender.com/"
+    },
+    {
+        title: "Flourush blog",
+        description: "A mini blog site that runs on express and an api with no database integration, just basic json file for data management and tailwind for styling and responsiveness.",
+        image: "public/img/flourushhome.png",
+        link: "https://flourish-blog.onrender.com"
+    },
+    {
+        title: "V-Stack landing page",
+        description: "A landing page for a saas product built with tailwind css and vanilla javascript for the dark mode toggle and responsiveness.",
+        image: "public/img/vstack.png",
+        link: "https://v-stack.onrender.com"
+    },
+    {
+        title: "Monomart e-commerce site",
+        description: "An e-commerce site built with laravel for the backend and tailwind css for styling and responsiveness and mysql for database management.",
+        image: "public/img/monomarthome.png",
+        link: "portfolio.html#project1"
+    }
+];
+
+const portfolioContainer = document.querySelector(".featured-projects");
+
+projects.forEach(project => {
+    const projectCard = `  <div
+    class="reveal bg-white rounded-xl overflow-hidden shadow-md hover:shadow-xl transition transform hover:scale-105">
+    <div class="h-64 overflow-y-auto">
+        <img src="${project.image}" class="w-full object-cover" alt="${project.title}" />
+    </div>
+    <div class="p-6">
+        <h3 class="text-xl font-semibold text-gray-900 mb-3">
+           ${project.title}
+        </h3>
+        <p class="text-gray-700 mb-4">
+            ${project.description}
+        </p>
+        <a href="${project.link}"
+            class="inline-block text-blue-600 hover:text-blue-700 font-semibold">View Details →</a>
+    </div>
+</div>`;
+    portfolioContainer.innerHTML += projectCard;
+});
